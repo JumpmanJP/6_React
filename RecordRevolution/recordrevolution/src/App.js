c7a12fa7a30464961edafd6c1d881df3036a69a3
JP O'Grady <jpogrady@JPs-MacBook-Pro.local>
2019-09-16 16:57:05 -0400

    react

---
+++ RecordRevolution/recordrevolution/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/NavBar/NavBar';
+import ViewOne from './components/View/ViewOne';
+import ViewTwo from './components/View/ViewTwo';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <ViewOne />
+      <h1 className='shopTitle' style={{textAlign: 'center', marginTop: '3em'}}>Shop Records!</h1>
+      <ViewTwo />
+    </div>
+  );
+}
+
+export default App;
---
+++ RecordRevolution/recordrevolution/src/components/View/ViewOne.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import Disc from '../../assets/disc copy.png';
+
+const Resize = styled.img`
+    width: 65vw;
+    height: auto;
+    display: block;
+    margin: 0 auto;
+`;
+
+const View = () => {
+
+    return(
+        <div>
+            <Resize src={Disc} />
+        </div>
+
+    );
+};
+
+export default View;
